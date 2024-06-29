bd786f1c6b7e98a384d80bc45e01e7716ebc14bf
shane-curtis <142857801+ShaneSaw1705@users.noreply.github.com>
2024-06-29 12:15:39 +1200

    user registry is on the bench

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { thread } from "@prisma/client";
+import Thread from "@/components/thread";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export default function Home() {
+export default async function Home() {
+  const posts: thread[] = await prisma.thread.findMany();
   return (
-    <div>Home</div>
-  )
+    <div className="flex flex-col items-center justify-start w-screen">
+      <h1 className="text-2xl">Threads</h1>
+      <div className="flex flex-col gap-3 w-full">
+        {posts.map((thread) => (
+          <Thread key={thread.id} post={thread} />
+        ))}
+      </div>
+    </div>
+  );
 }
